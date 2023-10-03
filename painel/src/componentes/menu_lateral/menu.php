42f7b348354801a97e4dc4411c9d0a7b9ad0530c
tamerelmenoufi <tamerelmenoufi@gmail.com>
2023-10-03 20:44:46 +0300

    Atualizações

--- painel/src/componentes/menu_lateral/menu.php
+++ painel/src/componentes/menu_lateral/menu.php
@@ -21,7 +21,7 @@
     <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
   </div>
   <div class="offcanvas-body">
-    <h4 style="color:#239ea0">Project - Painel de Controle</h4>
+    <h4 style="color:#055078">VNS - Painel de Controle</h4>
 
     <div class="row mb-1 menu-cinza">
       <div class="col">
